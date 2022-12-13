f2398b504056792a17e029b6e7b34f8ddbf85f42
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-13 11:05:16 +0800

    Created the boilerplate for the product page

---
+++ pages/product/index.tsx
@@ -0,0 +1,20 @@
+import Meta from "../../components/Meta"
+import Menu from "../../components/Menu"
+import Footer from "../../components/Footer"
+
+export default function Product() {
+    return (
+        <>
+            <Meta 
+                pageTitle="Products - Le REUSSI"
+                metaDescription="Test"
+                otherMetaData="Test"
+                metaKeywords="Test"
+                metaRobots="Test"
+            />
+            <Menu />
+            ASDASDASDASD
+            <Footer />
+        </>
+    )
+}
