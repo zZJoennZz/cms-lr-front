06099fbcf230ecb8af0a6e16c3f4ab74e809ec4d
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-13 11:04:22 +0800

    Created interface for product detail

--- types/appTypes.ts
+++ types/appTypes.ts
@@ -1,55 +1,82 @@
 interface PageList {
-    id: number,
-    pageName: string,
-    pageSlug: string,
+  id: number;
+  pageName: string;
+  pageSlug: string;
 }
 
 interface Pages {
-    pages: PageList[],
+  pages: PageList[];
 }
 
 interface Meta {
-    pageTitle: string,
-    metaDescription?: string,
-    metaRobots?: string,
-    metaKeywords?: string,
-    otherMetaData?: string, //this will be meta HTML tags
+  pageTitle: string;
+  metaDescription?: string;
+  metaRobots?: string;
+  metaKeywords?: string;
+  otherMetaData?: string; //this will be meta HTML tags
 }
 
 interface PageContent {
-    id: number,
-    internalName: string,
-    pageTitle: string,
-    pageSlug: string,
-    pageContent: PageComponent[], //this will be HTML format
-    isHome: boolean,
-    metaDescription?: string,
-    metaRobots?: string,
-    metaKeywords?: string,
-    otherMetaData?: string, //this will be meta HTML tags
-    isPublished: boolean,
+  id: number;
+  internalName: string;
+  pageTitle: string;
+  pageSlug: string;
+  pageContent?: PageComponent[]; //this will be HTML format
+  isHome: boolean;
+  metaDescription?: string;
+  metaRobots?: string;
+  metaKeywords?: string;
+  otherMetaData?: string; //this will be meta HTML tags
+  isPublished: boolean;
 }
 
 interface PageContent1 {
-    id: number,
-    internalName: string,
-    pageTitle: string,
-    pageSlug: string,
-    pageContent: string, //this will be HTML format
-    isHome: boolean,
-    metaDescription?: string,
-    metaRobots?: string,
-    metaKeywords?: string,
-    otherMetaData?: string, //this will be meta HTML tags
-    isPublished: boolean,
+  id: number;
+  internalName: string;
+  pageTitle: string;
+  pageSlug: string;
+  pageContent?: string; //this will be HTML format
+  isHome: boolean;
+  metaDescription?: string;
+  metaRobots?: string;
+  metaKeywords?: string;
+  otherMetaData?: string; //this will be meta HTML tags
+  isPublished: boolean;
 }
 
 interface PageComponent {
-    id: number,
-    name: string,
-    content: string,
-    isPublished: boolean;
+  id: number;
+  name: string;
+  content: string;
+  isPublished: boolean;
 }
 
+interface ProductDetail {
+  id: number;
+  slug: string;
+  productName: string;
+  description: string;
+  rating: number;
+  sku: string;
+  stocks: number;
+  isAvailable: boolean;
+  images: string[];
+  category: number[];
+}
 
-export type { PageList, Pages, Meta, PageContent, PageContent1, PageComponent }
+interface Category {
+  id: number;
+  slug: string;
+  categoryName: string;
+}
+
+export type {
+  PageList,
+  Pages,
+  Meta,
+  PageContent,
+  PageContent1,
+  PageComponent,
+  ProductDetail,
+  Category,
+};
