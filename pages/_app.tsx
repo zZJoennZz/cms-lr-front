6ae7928c507ee64f5dbbcc516acc2a2e3d3ad3f1
Joenn Aquilino <zzjoennzz@gmail.com>
2022-12-16 11:52:20 +0800

    Added the cart boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,8 +10,29 @@ const API_URL = "http://localhost:8000/"
 
 export default function App({ Component, pageProps }: AppProps,) {
   const { query } = useRouter()
-  // console.log(query)
+  console.log(query)
   const [user, setUser] = React.useState(null)
+  const [loginErr, setLoginErr] = React.useState("")
+
+  function addProductToCart() {
+    let cart: any = []
+    if (localStorage.getItem('cart')) {
+      cart = localStorage.getItem('cart')
+      cart = cart ? JSON.parse(cart) : []
+    }
+    cart.push({productId : 1, productName: 'test1'});
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }
+
+  function removeProductFromCart(productId: number) {
+    let storageCart: any;
+    // logic
+
+    storageCart = localStorage.getItem('cart')
+    storageCart = storageCart ? JSON.parse(storageCart) : []
+    let product = storageCart.filter((product : any) => product.productId !== productId)
+    localStorage.setItem('cart', JSON.stringify(product))
+  }
 
   async function checkIfLoggedIn() {
     await axios.post(`${API_URL}dbwebapi/vanguard/refresh/`, { refresh: localStorage.getItem("refresh") }, { headers: { Authorization: localStorage.getItem("token") } })
@@ -43,6 +64,9 @@ export default function App({ Component, pageProps }: AppProps,) {
       })
       .catch(err => {
         console.error(err.response.data.detail)
+        if (err.response.data.detail && err.response.data.detail === 'No active account found with the given credentials') {
+          setLoginErr(err.response.data.detail)
+        }
       })
   }
 
@@ -61,7 +85,15 @@ export default function App({ Component, pageProps }: AppProps,) {
 
   return (
     <UserContext.Provider value={user}>
-      <Component {...pageProps} login={(credentials : { username: string, password: string }) => login(credentials)} logout={() => logout()} />
+      <Component 
+        {...pageProps} 
+        login={(credentials : { username: string, password: string }) => login(credentials)} 
+        logout={() => logout()} 
+        addToCart={() => addProductToCart()} 
+        removeFromCart={(prodId: number) => removeProductFromCart(prodId)} 
+        loginErr={loginErr}
+        dismissLoginErr={() => setLoginErr('')}
+      />
     </UserContext.Provider>
   )
 }
